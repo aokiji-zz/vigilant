46cbf96f55f477ff872c714600813e0264deb2d2
aokiji-zz <mwtteixeira@gmail.com>
2025-07-10 08:50:44 -0300

    remove unused variable

--- vigilant-web/src/app/pages/import-data/ImportDataPage.tsx
+++ vigilant-web/src/app/pages/import-data/ImportDataPage.tsx
@@ -1,12 +1,8 @@
-import { useEffect, useState } from 'react'
 import Form from 'react-bootstrap/Form'
-import { useLocation, useNavigate } from 'react-router-dom'
 import '../vigilant-list/VigilantListPage.css'
 import logo from '../assets/nvigilant_logo_cropped.png'
 import ImportDataScan from './components/FileScanUpload'
 const ImportDataScanPage = () => {
-  const navigate = useNavigate()
-  const [sended, setSended] = useState<boolean>(false)
 
   return (
     <div className="container-vigilant">
